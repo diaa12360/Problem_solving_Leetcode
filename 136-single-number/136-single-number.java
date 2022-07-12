74bd00c218fdb95ea41ef4b6976b2409e7a11537
Deya Abu-Elwan <76823130+diaa12360@users.noreply.github.com>
2022-07-13 00:21:27 +0300

    Time: 1 ms (100.00%), Space: 49.6 MB (82.72%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -1,22 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int arr[] = new int[(int)((3 * 10000)/1.5)];
-        int arrn[] = new int[(int)((3 * 10000)/1.5)];
+        int ans = 0;
         for(int i = 0; i < nums.length; i++){
-            if(nums[i] < 0)
-                arrn[-nums[i]]++;
-            else
-                arr[nums[i]]++;
+            ans ^= nums[i];
         }
-        for(int i = 0; i < arr.length; i++){
-            if(arr[i] == 1){
-                return i;
-            }
-        }
-        for(int i = 0; i < arrn.length; i++){
-            if(arrn[i] == 1)
-                return -i;
-        }
-        return -1;
+        return ans;
     }
 }
